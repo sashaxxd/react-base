05cf599257080dcb71c6966f379fa22e6d68e3db
alex <webdizain@bk.ru>
2018-05-01 23:51:31 +0300

    1

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+class Header extends React.Component {
+    render() {
+        return React.createElement(
+            'h1',
+            null,
+            'dsfdsf'
+        );
+    }
+}
+
+ReactDOM.render(React.createElement(Header, null), document.getElementById('content'));
---
+++ jsx/script.jsx
@@ -0,0 +1,17 @@
+
+class Header extends React.Component{
+    render(){
+        return (
+            <h1>dsfdsf</h1>
+            )
+
+
+
+    }
+}
+
+ReactDOM.render(
+    <Header/>,
+
+    document.getElementById('content')
+);
